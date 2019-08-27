bf0eba0a47829c550cd6347e23bd5ade58ba6509
david-pok <david.pok@gmail.com>
2019-08-27 16:00:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
     <div className="container">
       <Logo />
       <div className="App">
+        hi
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
       </div>
     </div>
